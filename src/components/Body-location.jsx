0adba371bb779f96db4291795add8d3ffde174f7
Oxmoh <156785776+Oxmoh@users.noreply.github.com>
2024-04-08 16:12:53 +0200

    mise a jour

--- src/components/Body-location.jsx
+++ src/components/Body-location.jsx
@@ -1,22 +1,32 @@
-import { useState, useEffect } from 'react';
 import React from 'react'
 import "../style/location.css"
-import getHouse from "../utils/Fetch.js"
-import Card from '../utils/Card.js';
+import useFetch from "../utils/Fetch.js"
+import { Link } from 'react-router-dom';
 
 
 export default function Bodylocation() {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch
-            .get(getHouse)
-            .then((res) => setData(res.data));
-    }, []);
+    const { data, loading, error } = useFetch("/logements.json");
+
+    if (loading) return <div>Loading ...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    console.log(data);
+
     return (
         <div className='background'>
-            <div className='project'> {data.map((title, pictures, index) => (
-                <Card key={index} title={title} pictures={pictures} />
-            ))}
+            <div className="grid-container">
+                <ul className="galery">
+                    {data.map((item) => (
+                        <li key={item.id}>
+                            <Link to={`/House/${item.id}`}>
+                                <div className="item" appartment={item}>
+                                    <img src={item.cover} alt="appartement" />
+                                    <h3 className="title">{item.title}</h3>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
--- src/utils/Fetch.js
+++ src/utils/Fetch.js
@@ -1,11 +1,31 @@
-const getHouse = async () => {   
-    try {
-      const response = await fetch("../logements.json");
-      return response.ok ? await response.json() : null;
-    } catch (error) {
-      console.error("Une erreur est survenue", error);
-      return null;
-    }
-  };
+import { useState, useEffect } from "react";
 
-  
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Custom Hook pour récupérer les données de l'API qui sera utilisé dans les composants Product et Home. Données stockées dans le fichier data.json en attendant l'API.
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/logements.json");
+        if (!response.ok) throw new Error(response.statusText);
+
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
